2317d00834a7021d976421d616438ab0ce6283e7
wetfloo <yahahaha.1234567890@gmail.com>
2026-07-05 17:40:25 +0700

    extract README into a separate file, including it in the crate

---
+++ README.md
@@ -0,0 +1,8 @@
+`wetutil`: One-stop crate for all the [Rust](https://rust-lang.org/)
+features that I'm missing.
+
+This crate implements some useful extension traits
+for things like [`Result`] and [`Iterator`].
+
+It's mostly a mish-mash of things that
+I'm tired of writing in more tedious ways.
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
-//! `wetutil`: One-stop crate for all the rust features that I'm missing.
-//!
-//! This crate implements some useful extension traits
-//! for things like [`Result`] and [`Iterator`].
-//! It's mostly a mish-mash of things that I'm tired of writing in more tedious ways.
-
+#![doc = include_str!("../README.md")]
 #![deny(unreachable_pub)]
 
 pub mod convert;
